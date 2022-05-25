6d7a16887cf1186864cbf49101a5226e85cda8ed
Jonathan Price <jonprice0@gmail.com>
2022-05-25 12:42:41 -0400

    completed all models, associations and constraints

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,6 +5,13 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
+  Category.findAll({
+
+  }).then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
   // be sure to include its associated Products
 });
 
